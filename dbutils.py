93c54907cc9b136bc5da2ebdb54bfc4c9b9443b2
Richard Hawkes <richard_hawkes@yahoo.co.uk>
2018-11-11 17:59:18 +0000

    First commit

---
+++ dbutils.py
@@ -0,0 +1,56 @@
+import sqlite3
+
+class ServerDatabase:
+
+    def __init__(self):
+        self.conn = sqlite3.connect('serverdb')
+        self.conn.execute('pragma foreign_keys=on')
+        self.dbcursor = self.conn.cursor()
+
+    def setup_database(self):
+        self.create_server_table()
+
+    def create_server_table(self):
+        self.dbcursor.execute("drop table if exists server")
+        self.dbcursor.execute("drop table if exists server_member_system_alias")
+        self.dbcursor.execute("drop table if exists system")
+
+        self.dbcursor.execute("create table server (server_id text primary key, last_update_timestamp integer)")
+
+        self.dbcursor.execute("create table system (system_type text primary key)")
+        self.dbcursor.execute("replace into system (system_type) values (?)", ('ps4',))
+        self.dbcursor.execute("replace into system (system_type) values (?)", ('pc',))
+        self.dbcursor.execute("replace into system (system_type) values (?)", ('xbox',))
+
+        self.dbcursor.execute("create table server_member_system_alias (server_id text, member_id text, system_type text, system_alias text not null, primary key (server_id, member_id, system_type), foreign key (system_type) references system(system_type))")
+
+        self.conn.commit()
+
+    def update_server_member_system_alias(self, server_id, member_id, system_type, system_alias):
+        self.dbcursor.execute("replace into server_member_system_alias (server_id, member_id, system_type, system_alias) values (?, ?, ?, ?)", (server_id, member_id, system_type, system_alias))
+        self.conn.commit()
+        
+    def remove_server_member_system_alias(self, server_id, member_id, system_type):
+        if system_type == 'all':
+            self.dbcursor.execute("delete from server_member_system_alias where server_id = ? and member_id = ?", (server_id, member_id))
+        else:
+            self.dbcursor.execute("delete from server_member_system_alias where server_id = ? and member_id = ? and system_type = ?", (server_id, member_id, system_type))
+
+        self.conn.commit()
+
+    def get_all_server_member_system_aliases(self, server_id, system_type = 'all'):
+        if system_type == 'all':
+            self.dbcursor.execute("select * from server_member_system_alias where server_id = ? order by system_type", (server_id,))
+            return self.dbcursor.fetchall()
+        else:
+            self.dbcursor.execute("select * from server_member_system_alias where server_id = ? and system_type = ? order by system_type", (server_id, system_type))
+            return self.dbcursor.fetchall()
+        
+    def update_last_update_timestamp(self, server_id, timestamp):
+        self.dbcursor.execute("replace into server (server_id, last_update_timestamp) values (?, ?)", (server_id, timestamp))
+        self.conn.commit()
+
+    def get_last_update_timestamp(self, server_id):
+        self.dbcursor.execute("select last_update_timestamp from server where server_id = ?", (server_id,))
+        row = self.dbcursor.fetchone()
+        return row[0] if row is not None else None
---
+++ discord_bot.py
@@ -0,0 +1,149 @@
+import texttable
+import discord
+import dbutils
+import asyncio
+import random
+import pathlib
+import datetime
+import time
+
+# create discord client
+client = discord.Client()
+
+# Set file that will stop this process
+kill_file = pathlib.Path("/home/pi/discord/kill")
+
+last_server_update = {}
+ddb = dbutils.ServerDatabase()
+
+# token from https://discordapp.com/developers
+token = 'NDk4MTUxNzg3NzgyNDcxNjgy.DppwWA.57Ra07xIQijBpWVwcF3GDX4RsvU'
+
+def find_channel(server, channel_name):
+    for channel in server.channels:
+        if channel.name == channel_name:
+            return channel
+    return None
+
+def find_role(server, role_name):
+    for role in server.roles:
+        if role.name == role_name:
+            return role
+    return None
+
+def create_table(table_text):
+    try:
+        table = texttable.Texttable(0)
+        for row in table_text.split("|"):
+            table.add_row(row.split(","))
+        return "```" + table.draw() + "```"
+    except Exception as e:
+        raise(e)
+
+
+@client.event
+async def on_ready():
+    try:
+        # print bot information
+        print("username --> " + client.user.name)
+        print("userid --> " + client.user.id)
+        print('Discord.py Version --> {}'.format(discord.__version__))
+        print("Servers:")
+        for server in client.servers:
+            print("    {0} --> {1}".format(server.id, server.name))
+            last_server_update[server.id] = int(time.time())
+    except Exception as e:
+        print(e)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!table "):
+        try:
+            table_text = message.content[7:]
+            await client.send_message(message.channel, create_table(table_text))
+        except Exception as e:
+            await client.send_message(message.channel, "Error creating table: " + str(e))
+    elif message.content == "!members":
+        table_text = "MEMBER_NAME,JOINED_ON,STATUS"
+        for member in message.server.members:
+            # mlookup = discord.utils.find(lambda m: m.id == member.id, message.server.members)
+            table_text += "|{0},{1},{2}".format(member.name, member.joined_at.strftime("%Y-%m-%d %H:%M:%S"), member.status)
+        await client.send_message(message.channel, create_table(table_text))
+        print(message.server.default_role.mention)
+    elif message.content.startswith("!battle "):
+        thisplayer = message.author
+        adversary = message.content[8:]
+        adlookup = discord.utils.find(lambda m: m.mention == adversary, message.server.members)
+        if adlookup is None:
+            await client.send_message(message.channel, "```Usage: !battle <member>```")
+        else:
+            await client.send_message(message.channel, thisplayer.mention + " scores " + str(random.randint(1,100)) + ", " + adversary + " scores " + str(random.randint(1,100)))
+    elif message.content == '!aliases':
+        mems = ddb.get_all_server_member_system_aliases(message.server.id)
+        table_text = "MEMBER_ID,NICKNAME,SYSTEM,ALIAS"
+        removed_users = 0;
+        for mem in mems:
+            dmem = discord.utils.find(lambda m: m.id == mem[1], message.server.members)
+            if dmem is None:
+                # If a user has left (or was booted), then they would still exist
+                # in the database, but won't be found on the server. So remove them
+                # from the system aliases table altogether.
+                ddb.remove_server_member_system_alias(message.server.id, mem[1], 'all')
+                removed_users += 1
+            else:
+                table_text += "|{0},{1},{2},{3}".format(dmem.name, dmem.nick, mem[2], mem[3])
+        await client.send_message(message.channel, create_table(table_text))
+        if removed_users > 0:
+            await client.send_message(message.channel, "WARNING: " + str(removed_users) + " user(s) were removed from the table (left/kicked/booted?)")
+    elif message.content.startswith('!setalias'):
+        usage = "```USAGE: !setalias <member> <xbox|ps4|pc> <alias>```"
+        params = message.content.split()
+        if len(params) != 4:
+            await client.send_message(message.channel, usage)
+        elif discord.utils.find(lambda m: m.mention == params[1], message.server.members) is None:
+            await client.send_message(message.channel, "Could not find user " + params[1])
+            await client.send_message(message.channel, usage)
+        elif params[2] not in ('ps4', 'xbox', 'pc'):
+            await client.send_message(message.channel, "Invalid system '" + params[2] + "'. system must be xbox, ps4 or pc")
+            await client.send_message(message.channel, usage)
+        else:
+            dmem = discord.utils.find(lambda m: m.mention == params[1], message.server.members)
+            ddb.update_server_member_system_alias(message.server.id, dmem.id, params[2], params[3])
+            await client.send_message(message.channel, "Succesfully set user alias :smiley:")
+    elif message.content.startswith('!rmalias'):
+        usage = "```USAGE: !rmalias <member> <xbox|ps4|pc|all>```"
+        params = message.content.split()
+        if len(params) != 3:
+            await client.send_message(message.channel, usage)
+        elif discord.utils.find(lambda m: m.mention == params[1], message.server.members) is None:
+            await client.send_message(message.channel, "Could not find user '" + params[1] + "'")
+        elif params[2] not in ('ps4', 'xbox', 'pc','all'):
+            await client.send_message(message.channel, "Invalid system '" + params[2] + "'. system must be xbox, ps4, pc, or all")
+            await client.send_message(message.channel, usage)
+        else:
+            dmem = discord.utils.find(lambda m: m.mention == params[1], message.server.members)
+            ddb.remove_server_member_system_alias(message.server.id, dmem.id, params[2])
+            await client.send_message(message.channel, "Succesfully removed user :)")
+
+@client.event
+async def on_member_join(member):
+    # await client.send_message(general_chat_channel, "Marshal Mobot welcomes you " + member.mention + ". Now play nice y'hear?")
+    # await client.add_roles(member, (stranger_role))
+    pass
+
+async def maintenance_loop():
+    await client.wait_until_ready()
+    keep_running = True
+
+    while keep_running:
+        await asyncio.sleep(30)
+
+        if kill_file.is_file():
+            print("Kill file activated. Quitting.")
+            await client.logout()
+            kill_file.unlink() # unlink = delete file
+            keep_running = False
+
+# start bot
+client.loop.create_task(maintenance_loop())
+client.run(token)
