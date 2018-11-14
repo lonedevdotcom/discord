87f96be0a8fe6dd034cd52b9f91b4a43f2d91ad0
Richard Hawkes <richard_hawkes@yahoo.co.uk>
2018-11-14 08:24:48 +0000

    Storing key values in discord_config.py now

---
+++ discord_config.py
@@ -0,0 +1,8 @@
+# token from https://discordapp.com/developers
+DISCORD_OATH_TOKEN = '<YOUR DISCORD OATH TOKEN HERE>'
+
+# File to look for to trigger the Discord bot to stop gracefully.
+KILL_FILE = "/home/pi/discord/kill"
+
+# Location of the sqlite database file.
+SERVER_DB_FILE = "/home/pi/discord/serverdb"
